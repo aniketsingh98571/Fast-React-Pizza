acf806cf3564f84fa2435558a59f6122a56dbcef
Aniket Singh <thor98571@gmail.com>
2024-03-28 19:30:55 +0530

    feat:added applayout with loading and error states

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './Header.jsx'
+import CartOverview from '../features/cart/CartOverview'
+import {Outlet,useNavigation} from 'react-router-dom'
+import Loader from './Loader.jsx'
+export default function AppLayout() {
+    const navigation=useNavigation()
+    const isLoading=navigation.state==="loading"
+
+  return (
+    <div className='layout'>
+        {isLoading&&<Loader/>}
+        <Header/>
+        <main>
+            <Outlet/>
+        </main>
+        <CartOverview/>
+    </div>
+  )
+}
